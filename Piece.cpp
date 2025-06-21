82d397733b8c54cd49a48c0224fc0b2da8ac389c
Shaj2311 <haideralijafri2007@gmail.com>
2025-06-21 17:59:48 +0500

    Create base blueprint for Piece class

--- Piece.cpp
+++ Piece.cpp
@@ -1,1 +1,20 @@
 #include "Piece.h"
+
+const Vector2f Piece::PIECE_SIZE = Vector2f(1, 1);
+
+const Color Piece::RED = Color::Red;
+const Color Piece::ORANGE = Color(255, 165, 0);
+const Color Piece::GREEN = Color::Green;
+const Color Piece::BLUE = Color::Blue;
+const Color Piece::WHITE = Color::White;
+const Color Piece::YELLOW = Color::Yellow;
+
+void Piece::update()
+{
+	//change colors
+}
+
+void Piece::draw(RenderWindow& window)
+{
+	window.draw(piece);
+}
--- Piece.h
+++ Piece.h
@@ -5,5 +5,52 @@ using namespace sf;
 
 class Piece
 {
+private:
+	RectangleShape piece;
+	Color color;
+
+
+public:
+
+	static const Vector2f PIECE_SIZE;
+
+	static const Color RED;
+	static const Color ORANGE;
+	static const Color GREEN;
+	static const Color BLUE;
+	static const Color WHITE;
+	static const Color YELLOW;
+	
+
+	Piece()
+	{
+		piece.setFillColor(color);
+	}
+
+	inline void setColor(Color pieceColor)
+	{
+		if	(
+			pieceColor != RED &&
+			pieceColor != ORANGE &&
+			pieceColor != GREEN &&
+			pieceColor != BLUE &&
+			pieceColor != WHITE &&
+			pieceColor != YELLOW
+			)
+		{
+			return;
+		}
+
+		color = pieceColor;
+	}
+	inline void setPiece(RectangleShape otherPiece) { piece = otherPiece; }
+
+
+	inline Color getColor() { return color; }
+	inline RectangleShape& getPiece() { return piece; }
+
+
+	void update();
+	void draw(RenderWindow& window);
 };
 
