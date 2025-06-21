360f1bb885bd9c06f1453ccc0b789e98958e1b10
Shaj2311 <haideralijafri2007@gmail.com>
2025-06-21 19:02:29 +0500

    Create blueprint for Side class

--- Side.cpp
+++ Side.cpp
@@ -1,1 +1,18 @@
 #include "Side.h"
+
+void Side::update()
+{
+	//update pieces
+}
+
+void Side::draw(RenderWindow& window)
+{
+	//draw pieces
+	for (int i = 0; i < 3; i++)
+	{
+		for (int j = 0; j < 3; j++)
+		{
+			pieces[i][j].draw(window);
+		}
+	}
+}
--- Side.h
+++ Side.h
@@ -3,5 +3,30 @@
 
 class Side
 {
+	Piece pieces[3][3];
+
+	bool isSolved;
+
+public:
+	Side() {}
+
+	inline Piece& getPiece(unsigned int indexI, unsigned int indexJ)
+	{
+		if (indexI > 2 || indexJ > 2) return;
+		return pieces[indexI][indexJ];
+	}
+	inline bool getIsSolved() { return isSolved; }
+
+	
+	inline void setPiece(Piece p, unsigned int indexI, unsigned int indexJ)
+	{
+		if (indexI > 2 || indexJ > 2) return;
+		pieces[indexI][indexJ] = p;
+	}
+	inline void setIsSolved(bool solved) { isSolved = solved; }
+
+
+	void update();
+	void draw(RenderWindow& window);
 };
 
