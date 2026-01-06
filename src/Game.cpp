f9ae7e27dc088644c537cd39614b50a5e0d2ac0d
Shaj2311 <haideralijafri2007@gmail.com>
2026-01-06 23:19:00 +0500

    Missed some up/down states  in game update, added those

--- src/Game.cpp
+++ src/Game.cpp
@@ -258,6 +258,38 @@ void Game::pollEvents(GameState& currState, GameState& nextState)
 				{
 					break;
 				}
+			case MOVING_UP_1_UNSOLVED:
+				{
+					break;
+				}
+			case MOVING_UP_1_UNSOLVED_PAUSED:
+				{
+					break;
+				}
+			case MOVING_UP_2_UNSOLVED:
+				{
+					break;
+				}
+			case MOVING_UP_2_UNSOLVED_PAUSED:
+				{
+					break;
+				}
+			case MOVING_DOWN_1_UNSOLVED:
+				{
+					break;
+				}
+			case MOVING_DOWN_1_UNSOLVED_PAUSED:
+				{
+					break;
+				}
+			case MOVING_DOWN_2_UNSOLVED:
+				{
+					break;
+				}
+			case MOVING_DOWN_2_UNSOLVED_PAUSED:
+				{
+					break;
+				}
 			case IDLE_SOLVED:
 				{
 					break;
