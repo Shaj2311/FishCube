c82c8126d40b0424169448761684effb79531cf3
Shaj2311 <haideralijafri2007@gmail.com>
2026-01-10 10:36:15 +0500

    Create *sample* menus for required states

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -32,14 +32,172 @@ void drawMenu(RenderWindow& window, GameState& currState)
 				break;
 			}
 		case IDLE_UNSOLVED_PAUSED:
+			{
+				//Paused Text
+				Font titleFont("assets/PressStart2P-Regular.ttf");
+				Text titleText(titleFont, "PAUSED", 150);
+
+				titleText.setFillColor(Color::White);
+
+				FloatRect bounds = titleText.getLocalBounds();
+				titleText.setOrigin({
+						(bounds.position.x + bounds.size.x) / 2,
+						(bounds.position.y + bounds.size.y) / 2
+						});
+
+				titleText.setPosition({WINDOW_WIDTH/2.f, WINDOW_HEIGHT/3.f});
+
+
+				//Background
+				RectangleShape background({WINDOW_WIDTH, WINDOW_HEIGHT});
+				background.setFillColor(Color::Blue);
+
+
+				//render
+				window.draw(background);
+				window.draw(titleText);
+
+				break;
+			}
 		case MOVING_UNSOLVED_PAUSED:
-			break;
+			{
+				//Paused Text
+				Font titleFont("assets/PressStart2P-Regular.ttf");
+				Text titleText(titleFont, "PAUSED", 150);
+
+				titleText.setFillColor(Color::White);
+
+				FloatRect bounds = titleText.getLocalBounds();
+				titleText.setOrigin({
+						(bounds.position.x + bounds.size.x) / 2,
+						(bounds.position.y + bounds.size.y) / 2
+						});
+
+				titleText.setPosition({WINDOW_WIDTH/2.f, WINDOW_HEIGHT/3.f});
+
+
+				//Background
+				RectangleShape background({WINDOW_WIDTH, WINDOW_HEIGHT});
+				background.setFillColor(Color::Blue);
+
+
+				//render
+				window.draw(background);
+				window.draw(titleText);
+
+				break;
+			}
 		case IDLE_SOLVED_PAUSED:
+			{
+				//Paused Text
+				Font titleFont("assets/PressStart2P-Regular.ttf");
+				Text titleText(titleFont, "PAUSED", 150);
+
+				titleText.setFillColor(Color::White);
+
+				FloatRect bounds = titleText.getLocalBounds();
+				titleText.setOrigin({
+						(bounds.position.x + bounds.size.x) / 2,
+						(bounds.position.y + bounds.size.y) / 2
+						});
+
+				titleText.setPosition({WINDOW_WIDTH/2.f, WINDOW_HEIGHT/3.f});
+
+
+				//Background
+				RectangleShape background({WINDOW_WIDTH, WINDOW_HEIGHT});
+				background.setFillColor(Color::Blue);
+
+
+				//render
+				window.draw(background);
+				window.draw(titleText);
+
+				break;
+			}
 		case MOVING_SOLVED_PAUSED:
-			break;
+			{
+				//Paused Text
+				Font titleFont("assets/PressStart2P-Regular.ttf");
+				Text titleText(titleFont, "PAUSED", 150);
+
+				titleText.setFillColor(Color::White);
+
+				FloatRect bounds = titleText.getLocalBounds();
+				titleText.setOrigin({
+						(bounds.position.x + bounds.size.x) / 2,
+						(bounds.position.y + bounds.size.y) / 2
+						});
+
+				titleText.setPosition({WINDOW_WIDTH/2.f, WINDOW_HEIGHT/3.f});
+
+
+				//Background
+				RectangleShape background({WINDOW_WIDTH, WINDOW_HEIGHT});
+				background.setFillColor(Color::Blue);
+
+
+				//render
+				window.draw(background);
+				window.draw(titleText);
+
+				break;
+			}
 		case WIN:
-			break;
+			{
+				//Paused Text
+				Font titleFont("assets/PressStart2P-Regular.ttf");
+				Text titleText(titleFont, "YOU WIN", 150);
+
+				titleText.setFillColor(Color::White);
+
+				FloatRect bounds = titleText.getLocalBounds();
+				titleText.setOrigin({
+						(bounds.position.x + bounds.size.x) / 2,
+						(bounds.position.y + bounds.size.y) / 2
+						});
+
+				titleText.setPosition({WINDOW_WIDTH/2.f, WINDOW_HEIGHT/3.f});
+
+
+				//Background
+				RectangleShape background({WINDOW_WIDTH, WINDOW_HEIGHT});
+				background.setFillColor(Color::Blue);
+
+
+				//render
+				window.draw(background);
+				window.draw(titleText);
+
+				break;
+			}
 		case EXIT:
-			break;
+			{
+				//Paused Text
+				Font titleFont("assets/PressStart2P-Regular.ttf");
+				Text titleText(titleFont, "YOU SURE, BRUH?", 150);
+
+				titleText.setFillColor(Color::White);
+
+				FloatRect bounds = titleText.getLocalBounds();
+				titleText.setOrigin({
+						(bounds.position.x + bounds.size.x) / 2,
+						(bounds.position.y + bounds.size.y) / 2
+						});
+
+				titleText.setPosition({WINDOW_WIDTH/2.f, WINDOW_HEIGHT/3.f});
+
+
+				//Background
+				RectangleShape background({WINDOW_WIDTH, WINDOW_HEIGHT});
+				background.setFillColor(Color::Blue);
+
+
+				//render
+				window.draw(background);
+				window.draw(titleText);
+
+				break;
+			}
 	}
 }
